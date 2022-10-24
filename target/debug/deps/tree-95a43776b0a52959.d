d98b24e5e533d355f7788abfc7e6885cac65f454
Matz <mateus.assalti@gmail.com>
2022-10-24 15:30:58 -0300

    Update

--- target/debug/deps/tree-95a43776b0a52959.d
+++ target/debug/deps/tree-95a43776b0a52959.d
@@ -1,6 +1,6 @@
-/home/matz/Desktop/Code/Rust/tree/target/debug/deps/tree-95a43776b0a52959.rmeta: src/lib.rs src/bst.rs src/bst/binary_search_tree.rs
+/home/matz/Desktop/Code/Rust/Tree/target/debug/deps/tree-95a43776b0a52959.rmeta: src/lib.rs src/bst.rs src/bst/binary_search_tree.rs
 
-/home/matz/Desktop/Code/Rust/tree/target/debug/deps/tree-95a43776b0a52959.d: src/lib.rs src/bst.rs src/bst/binary_search_tree.rs
+/home/matz/Desktop/Code/Rust/Tree/target/debug/deps/tree-95a43776b0a52959.d: src/lib.rs src/bst.rs src/bst/binary_search_tree.rs
 
 src/lib.rs:
 src/bst.rs:
