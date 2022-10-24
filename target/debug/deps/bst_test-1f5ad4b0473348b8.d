740c472479c4f79efcd7ecba531edc31c915626f
Matz <mateus.assalti@gmail.com>
2022-10-24 15:49:39 -0300

    Added README

--- target/debug/deps/bst_test-1f5ad4b0473348b8.d
+++ target/debug/deps/bst_test-1f5ad4b0473348b8.d
@@ -1,5 +1,5 @@
-/home/matz/Desktop/Code/Rust/tree/target/debug/deps/bst_test-1f5ad4b0473348b8.rmeta: tests/bst_test.rs
+/home/matz/Desktop/Code/Rust/Tree/target/debug/deps/bst_test-1f5ad4b0473348b8.rmeta: tests/bst_test.rs
 
-/home/matz/Desktop/Code/Rust/tree/target/debug/deps/bst_test-1f5ad4b0473348b8.d: tests/bst_test.rs
+/home/matz/Desktop/Code/Rust/Tree/target/debug/deps/bst_test-1f5ad4b0473348b8.d: tests/bst_test.rs
 
 tests/bst_test.rs:
